23dc161ab534fdc1adcafbfae8daf63ce5c1b879
besteman <ellioy37@gmail.com>
2019-10-20 17:45:28 -0400

    send one response to front

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,7 +3,7 @@ const quotes_data = require("../data/quotes_array");
 module.exports = (app) => {
 
     app.get("/api/quotes_data", (req, res) => {
-        res.json(quotes_data);
+        res.json(quotes_data[0]);
     });
 
 };
