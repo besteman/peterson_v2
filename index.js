e8311c2fff4bb820d7c5af8933aa56b301549028
besteman <ellioy37@gmail.com>
2019-10-20 16:44:54 -0400

    Initial Setup

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+
+// require("./routes/apiRoutes")(app);
+require("./routes/html_routes")(app);
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
