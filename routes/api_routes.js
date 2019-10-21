b54d91d53ff4eb1c67385f5a28d2427f26e1d994
besteman <ellioy37@gmail.com>
2019-10-20 22:09:35 -0400

    Add cron job

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,7 +1,16 @@
+const cron = require('node-cron');
 const quotesData = require('../data/quotes_array');
+
+let numberOfQuote = 0;
+
+cron.schedule('* * * * *', () => {
+  // eslint-disable-next-line no-console
+  console.log('running a task every minute');
+  numberOfQuote += 1;
+});
 
 module.exports = (app) => {
   app.get('/api/quotes_data', (req, res) => {
-    res.json(quotesData[0]);
+    res.json(quotesData[numberOfQuote]);
   });
 };
