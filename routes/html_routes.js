10c6b8887856283ad20717339a5c1f62e0fc579f
besteman <ellioy37@gmail.com>
2019-10-20 22:46:58 -0400

    Add html route for about

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 module.exports = (app) => {
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/about.html'));
+  });
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
